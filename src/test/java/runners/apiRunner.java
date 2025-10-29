3eab430a456cb684670c9772f15c2525304fe78d
Esteban O. Caceres <114696998+Stven5490@users.noreply.github.com>
2025-10-28 23:44:41 -0300

    Carga Inicial

--- src/test/java/runners/apiRunner.java
+++ src/test/java/runners/apiRunner.java
@@ -1,4 +1,11 @@
 package runners;
 
+import com.intuit.karate.junit5.Karate;
+
 public class apiRunner {
+    @Karate.Test
+    Karate testAll() {
+        //Testear todas las features
+        return Karate.run().relativeTo(getClass());
+    }
 }
